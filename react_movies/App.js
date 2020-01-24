749dc5840bc8d0b7577ad1815dab5fc18322b06e
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-01-24 17:22:02 +0900

    2.0 Creating a Tab Navigation

--- react_movies/App.js
+++ react_movies/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { AppLoading, Asset } from "expo";
+import { AppLoading } from "expo";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { StyleSheet, Text, View } from "react-native";
+import TabNavigation from "./navigation/TabNavigation";
 import { render } from "react-dom";
 
 export default class App extends React.Component {
@@ -18,20 +18,13 @@ export default class App extends React.Component {
     await Font.loadAsync({
       ...Ionicons.font
     });
-    // await Asset.loadAsync([
-    //   require("image/icon.png")
-    // ]);
   };
 
   render() {
     const { loaded } = this.state;
 
     if (loaded) {
-      return (
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
-      );
+      return <TabNavigation />;
     } else {
       return (
         <AppLoading
@@ -43,12 +36,3 @@ export default class App extends React.Component {
     }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
