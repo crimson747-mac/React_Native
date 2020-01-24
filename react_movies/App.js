5bbff7359725a1b7730d1a742f5579425fa87560
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-01-24 14:31:43 +0900

    1.2 Pre Loading Assets

--- react_movies/App.js
+++ react_movies/App.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading, Asset } from "expo";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
+import { render } from "react-dom";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    loaded: false
+  };
+
+  handleError = error => console.log(error);
+
+  handleLoaded = () => this.setState({ loaded: true });
+
+  loadAsset = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+    // await Asset.loadAsync([
+    //   require("image/icon.png")
+    // ]);
+  };
+
+  render() {
+    const { loaded } = this.state;
+
+    if (loaded) {
+      return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      );
+    } else {
+      return (
+        <AppLoading
+          startAsync={this.loadAsset}
+          onFinish={this.handleLoaded}
+          onError={this.handleLoaded}
+        />
+      );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
