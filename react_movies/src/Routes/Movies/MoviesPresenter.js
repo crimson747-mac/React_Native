65312517fb3bf02445e9e9612f689f0c97af7bc7
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-01-27 00:10:33 +0900

    4.1 Loading Component

--- react_movies/src/Routes/Movies/MoviesPresenter.js
+++ react_movies/src/Routes/Movies/MoviesPresenter.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Text  } from "react-native";
 import PropTypes from "prop-types";
+import Loader from "../../components/Loader";
 
 //모든 스크린은 네비게이션 props를 가지고 있다.
-const MoviesPresenter = () => <Text>Movies</Text>;
+const MoviesPresenter = ({loading}) => loading ? <Loader/> :  <Text>Movies</Text>;
 
 MoviesPresenter.propTypes = {
     loading: PropTypes.bool.isRequired
