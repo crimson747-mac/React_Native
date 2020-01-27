8a6059ec18c09a742674f0d56382c9f2190e8ef9
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-01-27 13:56:56 +0900

    5.0 Making a Slider part One

--- react_movies/src/Routes/Movies/MoviesPresenter.js
+++ react_movies/src/Routes/Movies/MoviesPresenter.js
@@ -2,10 +2,19 @@ import React from "react";
 import { Text  } from "react-native";
 import PropTypes from "prop-types";
 import Loader from "../../components/Loader";
+import styled from "styled-components";
+import MovieSlider from "../../components/MovieSlider";
+import {BG_COLOR} from "../../../constants/Colors";
+
+const Container = styled.ScrollView`
+    background-color: ${BG_COLOR};
+`;
 
 //모든 스크린은 네비게이션 props를 가지고 있다.
 const MoviesPresenter = ({loading, upcoming, popular, nowPlaying}) => (
-    loading ? <Loader/> :  <Text>Movies</Text>
+    loading ? <Loader/> :  <Container>
+        <MovieSlider movies={nowPlaying}/>
+    </Container>
 );
 
 MoviesPresenter.propTypes = {
