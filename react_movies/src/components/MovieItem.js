7b4f7441e558c9b1151cfd06428e82c9304b3688
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-01-28 00:02:34 +0900

    6.3 Vertical Movies Part Two

--- react_movies/src/components/MovieItem.js
+++ react_movies/src/components/MovieItem.js
@@ -3,6 +3,7 @@ import PropTypes, { string } from "prop-types";
 import styled from "styled-components";
 import MovieRating from "./MovieRating";
 import MoviePoster from "./MoviePoster";
+import { GREAY_COLOR } from "../../constants/Colors";
 
 const Container = styled.View`
   align-items: center;
@@ -11,11 +12,51 @@ const Container = styled.View`
 
 const Title = styled.Text`
   color: white;
+  font-size:${props => (!props.big ? "16px" : "18px")}
   font-size: 13px;
 `;
 
-const MovieItem = ({ id, posterPhoto, title, voteAvg, horizontal = false }) =>
-  horizontal ? null : (
+const HContainer = styled.View`
+  margin-bottom: 20px;
+  flex-direction: row;
+`;
+
+const Column = styled.View`
+  margin-left: 20px;
+  width: 65%;
+`;
+
+const Overview = styled.Text`
+  color: ${GREAY_COLOR};
+  font-size: 14px;
+  margin-vertical: 10px;
+`;
+
+const MovieItem = ({
+  id,
+  posterPhoto,
+  title,
+  voteAvg,
+  horizontal = false,
+  overview
+}) =>
+  horizontal ? (
+    <HContainer>
+      <MoviePoster path={posterPhoto} />
+      <Column>
+        <Title big={true}>{title}</Title>
+        <MovieRating votes={voteAvg} />
+        {overview ? (
+          <Overview>
+            {" "}
+            {overview.length > 130
+              ? `${overview.substring(0, 127)}...`
+              : overview}
+          </Overview>
+        ) : null}
+      </Column>
+    </HContainer>
+  ) : (
     <Container>
       <MoviePoster path={posterPhoto} />
       <Title>{title.length > 10 ? `${title.substring(0, 7)}...` : title}</Title>
@@ -27,7 +68,8 @@ MovieItem.propTypes = {
   id: PropTypes.number.isRequired,
   posterPhoto: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  voteAvg: PropTypes.number.isRequired
+  voteAvg: PropTypes.number.isRequired,
+  overview: PropTypes.string
 };
 
 export default MovieItem;
